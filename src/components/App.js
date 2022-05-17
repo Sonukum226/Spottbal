7fc3c7801bd057fee7fb6596af67757f393fdc81
Sonukum226 <sonukum870@gmail.com>
2022-05-17 22:55:05 +0530

    Spottabl

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,16 @@ import React, { useState } from "react";
 
 import Header from "./header";
 import UserList from "./listingPage";
-import UserCard from "./UserCard";
 
 import data from "./MOCK_DATA.json";
 
 function App() {
-  const [newData, setnewData] = useState([]);
-
-  const dataHandler = (userId, Username) => {
-    setnewData((previousdata) => {
-      return [...previousdata, { id: userId, name: Username }];
-    });
-  };
-
   return (
     <div className="App-link">
-      <div>
-        <Header />
-      </div>
+      <Header />
 
       <div className="App-User">
-        <UserList
-          placeHolder="Add by Name or email"
-          data={data}
-          addData={dataHandler}
-        />
-
-        <div style={{ margin: "20px 0 0 10px" }}>
-          <UserCard />
-        </div>
+        <UserList placeHolder="Add by Name or email" data={data} />
       </div>
     </div>
   );
